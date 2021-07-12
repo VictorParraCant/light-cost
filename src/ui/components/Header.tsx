2eae3d4a34298a5927adbaa419f10714acd5f655
Victor Parra <victor.parra@cobee.io>
2021-07-12 13:48:03 +0200

    minimal feature in use

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {Text, useColorScheme, View, StyleSheet} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Header: React.FC<{title: string}> = ({title}) => {
+const Header: React.FC<{title: string; subtitle: string}> = ({
+  title,
+  subtitle,
+}) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.containerTitle}>
@@ -15,19 +18,32 @@ const Header: React.FC<{title: string}> = ({title}) => {
         ]}>
         {title}
       </Text>
+      <Text
+        style={[
+          styles.headerSubtitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {subtitle}
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   containerTitle: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginVertical: 64,
   },
   headerTitle: {
     fontSize: 32,
+    marginBottom: 12,
     fontWeight: '600',
+  },
+  headerSubtitle: {
+    fontSize: 24,
   },
 });
 
--- src/ui/screens/Home.tsx
+++ src/ui/screens/Home.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
@@ -10,6 +9,7 @@ import {
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {getCurrentCost} from '../../usecases/getCurrentCost';
 import Header from '../components/Header';
 import Section from '../components/Section';
 
@@ -19,29 +19,41 @@ const Home = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const currentCost = getCurrentCost(new Date());
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={[backgroundStyle, styles.container]}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header title="light cost?" />
-        <View
-          style={{
+      <View style={[backgroundStyle, styles.principalContent]}>
+        <Header title="light cost?" subtitle={currentCost} />
+      </View>
+      <View
+        style={[
+          styles.infoContent,
+          {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Next section">
-            Here the time of the{' '}
-            <Text style={styles.highlight}>next price change</Text> in the light
-            will be displayed
-          </Section>
-        </View>
-      </ScrollView>
+          },
+        ]}>
+        <Section title="Next feature">
+          Here the time of the{' '}
+          <Text style={styles.highlight}>next price change</Text> in the light
+          will be displayed
+        </Section>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  principalContent: {
+    flex: 0.8,
+  },
+  infoContent: {
+    flex: 0.2,
+  },
   highlight: {
     fontWeight: '700',
   },
